5d1fc110d5b05d2431774fb27cd865f74b592047
RafaellaSacramento <rsacramento@osas.com.br>
2026-05-18 13:21:59 -0300

    alterando cargos

--- Portal/Enums/Cargo.cs
+++ Portal/Enums/Cargo.cs
@@ -1,12 +1,40 @@
+using System.ComponentModel;
+
 namespace Portal.Enums
 {
     public enum Cargo
     {
-        Gerente,
-        Analista,
-        Assistente,
-        Coordenador,
-        Diretor,
-        Outro
+        [Description("AUX. DE MANUTENÇÃO PREDIAL")]
+        AuxManutencaoPredial,
+
+        [Description("AUX. DE ELETRICISTA")]
+        AuxEletricista,
+
+        [Description("AJUDANDE DE CARGA E DESCARGA")]
+        AjudanteCargaDescarga,
+
+        [Description("SERRALHEIRO/MARCENEIRO/ESTOFADOR")]
+        SerralheiroMarceneiroEstofador,
+
+        [Description("OFICIAIS DE MANUTENÇÃO PREDIAL")]
+        OficiaisManutencaoPredial,
+
+        [Description("TÉCNICO ELETRÔNICA")]
+        TecnicoEletronica,
+
+        [Description("TÉCNICO ELETRÔMECÂNICA")]
+        TecnicoEletromecânica,
+
+        [Description("TÉCNICO EDIFICAÇÃO")]
+        TecnicoEdificacao,
+
+        [Description("ENGENHEIROS")]
+        Engenheiros,
+
+        [Description("TÉCNICO INDUSTRIAL")]
+        TecnicoIndustrial,
+
+        [Description("ELETRICISTA")]
+        Eletricista,
     }
 }
