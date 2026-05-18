c9697b0762eca6e256f38453a396e5be65f59056
RafaellaSacramento <rsacramento@osas.com.br>
2026-05-18 02:33:51 -0300

    adaptando json para hospedagem

--- portal-frontend/vite.config.ts
+++ portal-frontend/vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portal-frontend/',
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: '/GestaoBandoDeHoras/'
 })
